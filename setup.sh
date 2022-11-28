4361f46175ad734bd12b764b4a251d395e69317b
serranomorante <serranomorantepatricio@gmail.com>
2022-11-28 06:19:07 +0000

    Nov 28, 2022, 1:19 AM

---
+++ setup.sh
@@ -0,0 +1,5 @@
+yum install wget
+mkdir pandoc
+wget -qO- https://github.com/jgm/pandoc/releases/download/2.19.2/pandoc-2.19.2-linux-amd64.tar.gz | \
+   tar xvzf - --strip-components 1 -C ./pandoc
+export PATH="./pandoc/bin:$PATH"
