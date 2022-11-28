33ae3f342e37f60f0a726ec616c6c84eaaa77901
serranomorante <serranomorantepatricio@gmail.com>
2022-11-28 16:42:04 +0000

    Nov 28, 2022, 11:42 AM

--- setup.sh
+++ setup.sh
@@ -5,8 +5,10 @@ yum -y install wget
 
 # Install EPEL
 wget https://copr-be.cloud.fedoraproject.org/results/petersen/pandoc/epel-7-x86_64/00976393-pandoc/pandoc-2.7.3-1.el7.x86_64.rpm
+wget https://copr-be.cloud.fedoraproject.org/results/petersen/pandoc/epel-7-x86_64/00976393-pandoc/pandoc-citeproc-0.16.2-1.el7.x86_64.rpm
 yum install pandoc-2.7.3-1.el7.x86_64.rpm
-yum install -y pandoc
+yum install pandoc-citeproc-0.16.2-1.el7.x86_64.rpm
+yum install -y pandoc pandoc-citeproc
 
 # yum -y install xz
 
