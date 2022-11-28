0251753d59c5b5a2c7521fb365a38f5ac1acfaa9
serranomorante <serranomorantepatricio@gmail.com>
2022-11-28 16:33:20 +0000

    Nov 28, 2022, 11:33 AM

--- setup.sh
+++ setup.sh
@@ -1,23 +1,31 @@
 echo "Starting setup.sh..."
 
-yum install wget
-yum install xz
-wget https://downloads.haskell.org/~ghc/9.2.5/ghc-9.2.5-src.tar.xz
-tar xf ghc-9.2.5-src.tar.xz
+yum -y install wget
+# yum -y install xz
 
-cd ghc-9.2.5
-./boot && ./configure
-hadrian/build -j
-cd ..
+# Install EPEL
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install ./epel-release-latest-*.noarch.rpm
+yum install -y pandoc pandoc-citeproc
+
+# yum -y install xz
+
+# wget https://downloads.haskell.org/~ghc/9.2.5/ghc-9.2.5-src.tar.xz
+# tar xf ghc-9.2.5-src.tar.xz
+
+# cd ghc-9.2.5
+# ./boot && ./configure
+# hadrian/build -j
+# cd ..
 
 # yum install -y which gmp-devel
 
-wget https://downloads.haskell.org/~cabal/cabal-install-3.8.1.0/cabal-install-3.8.1.0-x86_64-linux-deb10.tar.xz
-tar xf cabal-install-3.8.1.0-x86_64-linux-deb10.tar.xz
-ln -s cabal /usr/bin/cabal
+# wget https://downloads.haskell.org/~cabal/cabal-install-3.8.1.0/cabal-install-3.8.1.0-x86_64-linux-deb10.tar.xz
+# tar xf cabal-install-3.8.1.0-x86_64-linux-deb10.tar.xz
+# ln -s cabal /usr/bin/cabal
 
-cabal update
-cabal install pandoc pandoc-citeproc
+# cabal update
+# cabal install pandoc pandoc-citeproc
 
 # pandoc filters
 npm install --global mermaid-filter
