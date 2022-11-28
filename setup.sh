b7b5d5003851355c8f369f2b7274836a4a6f8392
serranomorante <serranomorantepatricio@gmail.com>
2022-11-28 14:00:08 +0000

    Nov 28, 2022, 9:00 AM

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
-yum update
-yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum install pandoc
-yum install pandoc-citeproc
-# yum install wget
-# mkdir pandoc
-# wget -qO- https://github.com/jgm/pandoc/releases/download/2.19.2/pandoc-2.19.2-linux-amd64.tar.gz | \
-#    tar xvzf - --strip-components 1 -C ./pandoc
+# yum makecache
+# yum -y install cabal-install
+
+# yum update
+# yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+# yum install pandoc
+# yum install pandoc-citeproc
+
+yum install wget
+mkdir pandoc
+wget -qO- https://github.com/jgm/pandoc/releases/download/2.19.2/pandoc-2.19.2-linux-amd64.tar.gz | \
+   tar xvzf - --strip-components 1 -C ./pandoc
+ln -s ./pandoc/bin/pandoc /usr/bin/pandoc
 # export PATH="./pandoc/bin:$PATH"
 echo "pandoc version -> $(pandoc --version)"
 # echo "which pandoc -> $(which pandoc)"
