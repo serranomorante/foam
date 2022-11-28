d29aac47daf67f38e1c90bca31d2dcb5aa33508d
serranomorante <serranomorantepatricio@gmail.com>
2022-11-28 07:24:09 +0000

    Nov 28, 2022, 2:24 AM

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
-yum install wget
-mkdir pandoc
-wget -qO- https://github.com/jgm/pandoc/releases/download/2.19.2/pandoc-2.19.2-linux-amd64.tar.gz | \
-   tar xvzf - --strip-components 1 -C ./pandoc
-export PATH="./pandoc/bin:$PATH"
+yum install pandoc
+# yum install wget
+# mkdir pandoc
+# wget -qO- https://github.com/jgm/pandoc/releases/download/2.19.2/pandoc-2.19.2-linux-amd64.tar.gz | \
+#    tar xvzf - --strip-components 1 -C ./pandoc
+# export PATH="./pandoc/bin:$PATH"
 echo "pandoc version -> $(pandoc --version)"
-echo "which pandoc -> $(which pandoc)"
-echo "pandoc path -> $PANDOC_PATH"
-echo "$(pwd)/pandoc/bin" > /etc/profile.d/pndc.sh
-chmod +x /etc/profile.d/pndc.sh
-. /etc/profile
-cat /etc/profile.d/pndc.sh
-echo "path --> $PATH"
+# echo "which pandoc -> $(which pandoc)"
+# echo "pandoc path -> $PANDOC_PATH"
+# echo "$(pwd)/pandoc/bin" > /etc/profile.d/pndc.sh
+# chmod +x /etc/profile.d/pndc.sh
+# . /etc/profile
+# cat /etc/profile.d/pndc.sh
+# echo "path --> $PATH"
