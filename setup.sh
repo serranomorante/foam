4edf76011a8e98c030e978ff2ea84f9bf5ce0874
serranomorante <serranomorantepatricio@gmail.com>
2022-11-28 06:52:18 +0000

    Nov 28, 2022, 1:52 AM

--- setup.sh
+++ setup.sh
@@ -3,4 +3,6 @@ mkdir pandoc
 wget -qO- https://github.com/jgm/pandoc/releases/download/2.19.2/pandoc-2.19.2-linux-amd64.tar.gz | \
    tar xvzf - --strip-components 1 -C ./pandoc
 export PATH="./pandoc/bin:$PATH"
-which pandoc
+echo "which pandoc -> $(which pandoc)"
+echo 'pathmunge /vercel/path0/pandoc/bin' > /etc/profile.d/pndc.sh
+chmod +x /etc/profile.d/pndc.sh
