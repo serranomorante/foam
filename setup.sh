eacad3886cc760e3a2bf9f920790759a996e1069
serranomorante <serranomorantepatricio@gmail.com>
2022-11-28 07:36:30 +0000

    Nov 28, 2022, 2:36 AM

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
+yum update
 yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum install pandoc
+yum install pandoc-citeproc
 # yum install wget
 # mkdir pandoc
 # wget -qO- https://github.com/jgm/pandoc/releases/download/2.19.2/pandoc-2.19.2-linux-amd64.tar.gz | \
